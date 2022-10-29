0404b5fac7a5e2e1711d2af3fb1816ceede6f9b2
alexrc-dev <50207586+alexrc-dev@users.noreply.github.com>
2022-10-28 21:53:39 -0500

    Changed Logos, Colors and Settings to all Richie

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -307,7 +307,7 @@ class Base(StyleguideMixin, DRFMixin, RichieCoursesConfigurationMixin, Configura
     }
 
     # Internationalization
-    TIME_ZONE = "Europe/Paris"
+    TIME_ZONE = "US/Central"
     USE_I18N = True
     USE_L10N = True
     USE_TZ = True
